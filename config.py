ccd53f3b00cbe323d4389c942e88bc579695ccfc
alecsei1982h <127231945+alecsei1982h@users.noreply.github.com>
2023-05-30 01:01:56 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = "6060180566:AAFXrbGbuvd6b39sHUBkkjWYlVenCywb8ko"
+
+keys = {
+    'Рубль': 'RUB',
+    'Евро': 'EUR',
+    'Доллар': 'USD',
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'доллар': 'USD',
+}
